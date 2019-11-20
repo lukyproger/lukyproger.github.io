9612c0f6753a09ad5eb9d95afd90e31634545bc8
unknown <newyork1707@gmail.com>
2019-11-20 14:21:09 +0200

    fix html source path

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('css', function() {
 });
 
 gulp.task('html', function() {
-  return gulp.src('src/*.html').pipe(browserSync.reload({ stream: true }));
+  return gulp.src('*.html').pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('script', function() {
@@ -48,30 +48,18 @@ gulp.task('script', function() {
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: 'src/'
+      baseDir: '.'
     }
   });
 });
 
-gulp.task('export', function() {
-  let buildHtml = gulp.src('src/**/*.html').pipe(gulp.dest('dist'));
-
-  let BuildCss = gulp.src('src/css/**/*.css').pipe(gulp.dest('dist/css'));
-
-  let BuildJs = gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'));
-
-  let BuildFonts = gulp.src('src/fonts/**/*.*').pipe(gulp.dest('dist/fonts'));
-
-  let BuildImg = gulp.src('src/img/**/*.*').pipe(gulp.dest('dist/img'));
-});
 
 gulp.task('watch', function() {
   gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('src/*.html', gulp.parallel('html'));
+  gulp.watch('*.html', gulp.parallel('html'));
   gulp.watch('src/js/*.js', gulp.parallel('script'));
 });
 
-gulp.task('build', gulp.series('clean', 'export'));
 
 gulp.task(
   'default',
